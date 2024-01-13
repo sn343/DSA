a0e86e0c8515fb0c8b06344163e75b79272561ac
Shubham Nikam <shubhamarunnikam2003@gmail.com>
2024-01-13 12:20:12 +0530

    code update

--- f.cpp
+++ f.cpp
@@ -1,28 +1,68 @@
 #include <iostream>
-#include<vector>
-#include<limits.h>
 using namespace std;
 
-int solve(vector<int>& arr,int target){
-  if (target == 0){
-    return 0;
+void merge(int *arr, int s, int e){
+int mid = (s+e)/2;
+
+int len1 = mid-s+1;
+int len2 =e-mid;
+
+int* left = new int[len1];
+int* right = new int[len2];
+
+int k = s;
+for(int i = 0; i<len1; i++){
+  left[i]=arr[k];
+  k++;
+}
+k = mid+1;
+for(int i = 0; i<len2; i++){
+  right[i]=arr[k];
+  k++;
+}
+
+int leftIndex = 0;
+int rightIndex = 0;
+int mainArrayIndex = s;
+
+while(leftIndex<len1 && rightIndex<len2){
+  if(left[leftIndex]<right[rightIndex]){
+    arr[mainArrayIndex++] = left[leftIndex++];
   }
-  if(target<0){
-    return INT_MAX;
+  else{
+    arr[mainArrayIndex++] = right[rightIndex++];
   }
-  int mini = INT_MAX;
-  for(int i =0; i<arr.size(); i++){
-    int ans = solve(arr ,target-arr[i]);
-    if(ans != INT_MAX)
-    mini = min(mini,ans + 1);
-  }
-  return mini;
+}
+while(leftIndex<len1){
+  arr[mainArrayIndex++]=left[leftIndex++];
+}
+while(rightIndex<len2){
+  arr[mainArrayIndex++]=right[rightIndex++];
+}
+}
+
+void mergeSort(int arr[],int e, int s){
+  if(s>=e)
+  return;
+
+  int mid = (s+e)/2;
+  mergeSort(arr,s,mid);
+
+  mergeSort(arr,e,mid+1);
+
+  merge(arr,s,e);
 }
 
 int main() {
-  vector<int> arr{1,2};
-  int target = 5;
-  int ans = solve(arr,target);
-  cout<<"ans is "<<ans<<endl;
+  int arr [] = {4,13,5,13,2,12,2,2,2,2,2,2};
+  int n = 12;
+  int s = 0;
+  int e = n-1;
+
+  mergeSort(arr ,e, s);
+  for(int i =0; i<n; i++){
+    cout<<arr[i]<<" ";
+  }
+  cout<<endl;
   return 0;
 }
