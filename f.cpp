4aae22bc01bb44e80f2c5155c12b32c53067c898
Shubham Nikam <shubhamarunnikam2003@gmail.com>
2024-01-23 20:04:37 +0530

    update

--- f.cpp
+++ f.cpp
@@ -26,7 +26,7 @@ int main(void) {
   int array[] = {3, 4, 5, 6, 7, 8, 9};
   int x = 4;
   int n = sizeof(array) / sizeof(array[0]);
-  int result = binarySearch(array, x, 0, n - 1);
+  int result = binarySearch(array, x, 0, n - 1); 
   if (result == -1)
     printf("Not found");
   else
