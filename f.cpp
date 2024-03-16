159389187e5e738d81c994c814e659065b80e556
Shubham Nikam <shubhamarunnikam2003@gmail.com>
2024-03-16 19:08:24 +0530

    update

--- f.cpp
+++ f.cpp
@@ -1,35 +1,25 @@
-// Binary Search in C++
-
-#include <iostream>
+#include<iostream>
+#include<vector>
 using namespace std;
-  
-int binarySearch(int array[], int x, int low, int high) {
-  
-	// Repeat until the pointers low and high meet each other
-  while (low <= high) {
-    int mid = low + (high - low) / 2;
 
-    if (array[mid] == x)
-      return mid;      
-
-    if (array[mid] < x)   
-      low = mid + 1;    
-
-    else
-      high = mid - 1;
-      
-  } 
-       
-  return -1;  
+int main(){
+    vector<int> arr{10,20,30,40,50,60,70,80,90,100};
+    int sum = 100;
+    
+    for(int i=0; i<arr.size(); i++){
+        int element1 = arr[i];
+        for(int j=i+1; j<arr.size(); j++){
+            int element2 = arr[j];
+            for(int k=j+1; k<arr.size(); k++){
+                int element3 = arr[k];
+                
+                if(element1+element2+element3 == sum){
+                    cout<<element1<<","<<element2<<","<<element3<<" "<<endl; 
+                }
+            }
+        }
+    }
 }
-// main function 
-int main(void) {
-  int array[] = {3, 4, 5, 6, 7, 8, 9};  
-  int x = 4;
-  int n = sizeof(array) / sizeof(array[0]);
-  int result = binarySearch(array, x, 0, n - 1); 
-  if (result == -1)
-    printf("Not found");
-  else
-    printf("Element is found at index = %d", result);
-}
+
+
+ 
